f512945f541e1d6a12f183750c70b947740ce183
luke.doll <31712880+ludoll@users.noreply.github.com>
2017-11-21 19:20:50 -0600

    Final Commit

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -22,6 +23,31 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "/results")
+    public String searchByType(Model model, @RequestParam String searchType,
+                               @RequestParam String searchTerm) {
+        model.addAttribute("columns", ListController.columnChoices);
 
+        switch (searchType) {
+            case "all":
+                if (searchTerm.isEmpty()) {
+                    model.addAttribute("searchData", JobData.findAll());
+                } else {
+                    model.addAttribute("searchData", JobData.findByValue(searchTerm));
+                }
+
+                break;
+            case "position type":
+            case "core competency":
+            case "employer":
+            case "location":
+                model.addAttribute("searchData", JobData.findByColumnAndValue(searchType, searchTerm));
+                break;
+
+
+        }
+        return "search";
+        // TODO #1 - Create handler to process search request and display results
+
+    }
 }
